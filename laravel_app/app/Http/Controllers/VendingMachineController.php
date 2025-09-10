be79fa0728e7d5a9527479b8bd36e64929636e1c
YUSEMAN <authority1001x@gmail.com>
2025-09-10 19:01:44 +0900

    Add VendingMachineController with index, show, and nearby methods

--- laravel_app/app/Http/Controllers/VendingMachineController.php
+++ laravel_app/app/Http/Controllers/VendingMachineController.php
@@ -3,8 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\VendingMachine;
 
 class VendingMachineController extends Controller
 {
-    //
+    public function index()
+    {
+        return VendingMachine::with('inventories.product')->get();
+    }
+
+    public function show($id)
+    {
+        return VendingMachine::with('inventories.product')->findOrFail($id);
+    }
+
+    public function nearby(Request $request)
+    {
+        $lat = $request->input('lat');
+        $lng = $request->input('lng');
+
+        return VendingMachine::selectRaw(
+            "*, (6371 * acos(cos(radians(?)) * cos(radians(latitude)) 
+            * cos(radians(longitude) - radians(?)) 
+            + sin(radians(?)) * sin(radians(latitude)))) AS distance",
+            [$lat, $lng, $lat]
+        )
+        ->orderBy('distance')
+        ->limit(5)
+        ->get();
+    }
 }
