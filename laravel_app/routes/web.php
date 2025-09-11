d74d090c0c3cc5639e751c44a72730a77bdd506e
YUSEMAN <authority1001x@gmail.com>
2025-09-11 12:48:59 +0900

    Fix models: restore VendingMachine and Product with relationships

--- laravel_app/routes/web.php
+++ laravel_app/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VendingMachineController;
 
 Route::get('/', function () { return view('welcome'); });
+Route::get('/vending-machines/nearby', [VendingMachineController::class, 'nearby'])
+    ->name('vending-machines.nearby');
 Route::get('/vending-machines', [VendingMachineController::class, 'index']);
 Route::get('/vending-machines/{id}', [VendingMachineController::class, 'show']);
-Route::get('/vending-machines/nearby', [VendingMachineController::class, 'nearby']);
